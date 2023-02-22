26a41d8e5300f11db2ef6329c4ff5273fdba7022
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-22 13:57:05 -0800

    ex3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - This is the main program
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,12 @@
-
+/**
+ * print_alphabet - prints lowwer letters from a-z
+ * Return: void
+ */
+void print_alphabet(void)
+{	
+	for (int alpha =  97; alpha < 122; alpha ++)
+	{
+	printf(char(alpha));
+	}
+	printf('\n");
+}
