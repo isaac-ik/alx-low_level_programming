6b2e929f7c8f198f82e22746b45831488dad3883
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-22 11:16:04 -0800

    use the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/*First c program*/
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return (0);
+}
