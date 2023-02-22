47a050d001b3803da314a64f1f3b859f8acb8a91
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-22 11:06:57 -0800

    use the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 /*First c program*/
 #include <stdio.h>
-int main()
+/**
+ * main - This is the main function
+ * Return: 0
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
