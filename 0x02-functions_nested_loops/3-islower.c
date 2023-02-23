b66b63fcd062d8ef56f17e1f30f81d97791456d1
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-02-23 20:57:48 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,16 @@
-
+#include "main.h"
+/**
+* is_lower - checks for lowercase
+* Return: 1 if lowercase and 0 if otherwise
+*/
+int is_lower(int c)
+{
+  if (c >=97 && c <= 122)
+  {
+    return (1);
+  }
+  else
+  {
+    return (0);
+  }
+}
