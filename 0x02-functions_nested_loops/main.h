ffca4e22a8b9e424d84381ca9b71597e70416518
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-02-23 14:16:45 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * print_alphabet - prints lowwer letters from a-z
  * Return: void
@@ -8,7 +20,6 @@ void print_alphabet(void)
 	int alpha = 97;
 	for (; alpha < 122; alpha++)
 	{
-	printf("%c",alpha);
+	_putchar(alpha);
 	}
-	printf("\n");
 }
