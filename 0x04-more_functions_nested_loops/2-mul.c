97897d9e1cf8e622d19c5653bfe58484970a1bf6
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-02-23 23:36:25 +0100

    3rd excercise

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,13 @@
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first input of the function
+ * @b: second input
+ *
+ * Return: return results
+*/
 
+int mul(int a, int b)
+{
+	return (a * b);
+}
