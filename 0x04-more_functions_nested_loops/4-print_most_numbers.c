22857f8cb3fe258eaff2a15cb893e69abb595ea2
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-02-23 23:41:35 +0100

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,8 @@
 #include"main.h"
 
 /**
- * print_most_numbers - print 0 - 9 apart
- *                 from 2 and 4 and you can
- *                 only use _putchar twice
- * Return: Always 0 (Success)
+ * print_most_numbers - print 0 - 9 apart from 2 and 4
+ * Return: 0
 */
 
 void print_most_numbers(void)
