cb2c94791239d3cc03444fed192e2dce4c769514
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-23 12:02:01 -0800

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,12 @@
 */
 int is_lower(int c)
 {
-  if (c >=97 && c <= 122)
-  {
-    return (1);
-  }
-  else
-  {
-    return (0);
-  }
+	if (c >=97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
