4da6ee0dbb30d817804f82b2d7bfd5c8167465cf
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-27 05:39:40 -0800

    Excercise 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps two integers
+ *
+ * @a: the first integers
+ * @b: the second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int v;
+	v = a;
+	a = b;
+	b = v;
+}
