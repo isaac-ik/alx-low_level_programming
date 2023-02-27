d4815ffd45d3dfb31401802d39e4f323e41a380d
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-27 07:11:18 -0800

    Excercise 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/**
+ * _puts - prints out a string
+ * @str: string to be printed out
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
+}
