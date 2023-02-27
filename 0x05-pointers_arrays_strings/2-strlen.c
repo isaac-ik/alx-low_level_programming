47ee856baafb2199de9ad2858382a1c494b3d4cf
isaac-ik <isaac.ikpemi@gmail.com>
2023-02-27 06:05:59 -0800

    Excercise 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - return the length of a string
+ *
+ * @s - strings whose length will be returned
+ *
+ * Return: 0
+ */
+int _strlen(char *c)
+{
+	int length;
+	while (*c == "\0")
+	{
+		length = length + 1;
+		*(c++)
+	}
+}
