f9cc9eb3a367225f6fa80109f34300e555427350
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-03-06 15:01:37 +0100

    ex 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different combinations of two two-digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
     int i = 0, j = 0;
