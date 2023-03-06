14995de0b527b8e8416c4d7714cf192fa80cf9ff
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-03-06 15:14:35 +0100

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,17 +21,18 @@ int main(void)
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
+			if (i == 98 && j == 99)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
 		}
-		if (i == 98 && j == 99)
-		{
-			putchar('\n');
-		}
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		j++;
+		i++;
 	}
-	i++;
+	return (0);
 }
