f3acc44de48506ba3ce27cf9c2529ded9254d492
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-03-09 19:47:11 +0100

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+/**
+ * factorial - returns the factorial of a number
+ * @n: a number
+ *
+ * Return: if 0, return 1. if less than 0, return -1. else return the factoiral
+ */
+
+int factorial(int n)
+{
+	int v;
+	
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	v = n * factorial(n-1);
+	n--;
+	return (v);
+}
