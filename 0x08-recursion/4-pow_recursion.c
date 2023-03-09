7a6384ca84b223304d677cf43e876c8573278d83
isaac-ik <isaac.ikpemi@gmail.com>
2023-03-09 11:05:26 -0800

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,18 +8,17 @@
 
 int _pow_recursion(int x, int y)
 {
-    int power;
+	int power;
 
-    if (y < 0)
-    {
-        return (-1);
-    }
-    else if (y == 0)
-    {
-        return (1);
-    }
-    power = x * _pow_recursion(x, y-1);
-
-    return (power);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	power = x * _pow_recursion(x, y - 1);
+	return (power);
 }
 
