da7ae6e8ab31472a524905f39ef28350c634e7f8
isaac-ik <isaac.ikpemi@gmail.com>
2023-03-10 10:14:47 -0800

    Ex1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - program to print its name
- * @argc - argument count
- * @argv - array of pointers to parameters
+ * @argc: argument count
+ * @argv: array of pointers to parameters
  *
- * Retrun: 0
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
