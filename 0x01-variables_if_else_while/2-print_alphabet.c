0dc4ec27cb7287aae0da62b3ae86f9dd8771167c
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-06 19:00:30 -0700

    making iteration

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - the code goes down there
+  *
+  * Description: prints the alphabets in lowercase
+  * Return: always 0
+  */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar(10);
+	return (0);
+}
