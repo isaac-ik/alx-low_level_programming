0dc1cec5fb2e2c2e99ef209337fa132888c84033
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-06 20:07:02 -0700

    making decisions and iterations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,16 +13,16 @@ int main(void)
 
 	for (num = 48; num <= 57; num++)
 	{
+		putchar(num);
 		if (num == 57)
 		{
-			putchar(num);
 		}
 		else
 		{
-			putchar(num);
 			putchar(44);
 		}
 		putchar(32);
 	}
+	putchar(10);
 	return (0);
 }
