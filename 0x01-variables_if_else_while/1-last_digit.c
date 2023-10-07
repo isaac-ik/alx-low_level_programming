33553b7848e6bf04e4358891628ce3573277735e
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-06 18:29:49 -0700

    making decision and iteration

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 == 0)
 	{
-		printf("and is 0\n");
+		printf("Last digit of %d is %d and is 0\n");
 	}
 	else if (n % 10 > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n");
 	}
 	/* your code goes there */
 	return (0);
