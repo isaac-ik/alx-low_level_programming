78d05fa0913699f5807c2301cb2dc6bb16448b41
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-10 12:35:05 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * times_table - prints the times table
+  * owner/programmer: isaac-ik
+  * return: nothing
+  */
+void times_table(void)
+{
+	int num, xnum, multip;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (xnum = 0; xnum <= 9; xnum++)
+		{
+			multip = num * xnum;
+			if (multip < 10)
+			{
+				_putchar(multip + '0');
+			}
+			else if (multip >= 10)
+			{
+				_putchar((multip / 10) + '0');
+				_putchar((multip % 10) + '0');
+			}
+			else
+			{
+				_putchar(48);
+			}
+
+			if (xnum < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(10);
+			}
+		}
+	}
+}
