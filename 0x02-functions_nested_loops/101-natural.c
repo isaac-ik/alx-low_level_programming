07495eb9e7061bb66d62b99df0f8930bc2d66c27
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-11 16:53:57 -0700

    functions and loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - a program that computes and prints 
+  * the sum of all the multiples of 3 or 5 below 1024 (excluded)
+  * 
+  * Programmer: Isaac-ik
+  * Return: 0
+  */
+int main(void)
+{
+	int n, sum;
+
+	/* sets the range of the checks and computation */
+	for (n = 1; n < 1024; n++)
+	{
+		sum = 0;
+		/**
+		  * perform the computation for only number
+		  * divisible by 3 or 5
+		  */
+		if ((n % 3 == 0) || (n % 5) == 0)
+		{
+			sum = sum + n;
+		}
+		else
+		{
+			sum = sum + 0;
+		}
+	}
+	printf("%d\n",sum);
+	return (0);
+}
