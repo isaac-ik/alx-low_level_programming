2f1d412ebbde6ed6824bb1db15fd6c50b78e2a66
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-11 09:35:47 -0700

    functions and loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - a function to add two integers
+  * @x: first number
+  * @y: second number
+  *
+  * Owner/Programmer: Isaac-ik
+  * Return: the result
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
