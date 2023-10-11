359025952e0eb7f98e02c945cf033e38e81c4287
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-11 16:02:01 -0700

    functions and loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,17 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
+		_putchar(((n * -1) % 10) + '0');
 		return ((n * -1) % 10);
 	}
 	else if (n > 0)
 	{
+		_putchar((n % 10) + '0');
 		return (n % 10);
 	}
 	else
 	{
+		_putchar(0 + '0');
 		return (0);
 	}
 }
