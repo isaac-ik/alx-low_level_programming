b7c5141e7960b44cae421b31e3dd6017868112a7
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-11 17:19:13 -0700

    functions and loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - a program that prints the first 50 Fibonacci numbers, starting with 1 and 2
+  * Programmer: Isaac-ik
+  * Retuurn: 0
+  */
+int main(void)
+{
+	int count, fN, sN, tN;
+
+	count = 0;
+	fN = 1;
+	sN = 2;
+	while (count < 49)
+	{
+		printf("%d, ", fN);
+		tN = fN + sN;
+		fN = sN;
+		sN = tN;
+		count = count + 1;
+	}
+	printf("\n");
+	return (0);
+}
