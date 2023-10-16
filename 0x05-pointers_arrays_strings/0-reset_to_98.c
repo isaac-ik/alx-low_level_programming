5916d5c282876a856185582f166cb9f6ac046e41
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-16 05:24:55 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset-to_98 - function
+  * @pn: a pointer 
+  * Description: takes a pointer to an int as parameter 
+  * and updates the value it points to to 98
+  * Programmer:Isaac-ik
+  * Return: nothing
+  */
+void reset_to_98(int *pn)
+{
+	*pn = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *pn);
+void swap_int(int *pa, int *pb);
+int _strlen(char *s);
+
+#endif /* MAIN_H */
