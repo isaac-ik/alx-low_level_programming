813c7a9b365493453325fc318d627a746171cca3
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-10-16 15:58:42 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,3 +22,25 @@ void puts_half(char *pstr)
 	}
 	_putchar('\n');
 }
+/**
+  * _strlen - a function
+  * @ps: a pointer to a string
+  * Description: returns the length of a string
+  * Programmer: Isaac-ik
+  * Return: length of the string
+  */
+int _strlen(char *ps)
+{
+	int length;
+	char *pi;
+
+	length = 0;
+	pi = ps;
+
+	while (*pi != '\0')
+	{
+		length = length + 1;
+		pi = pi + 1;
+	}
+	return (length);
+}
