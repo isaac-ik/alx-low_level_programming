08e44bb9019d32c52112b0f426846499a0429dc9
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-16 05:57:49 -0700

    pointer and string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,14 @@
 int _putchar(char c);
 void reset_to_98(int *pn);
 void swap_int(int *pa, int *pb);
-int _strlen(char *s);
+int _strlen(char *ps);
+void _puts(char *pstr);
+void print_rev(char *ps);
+void rev_string(char *ps);
+void puts2(char *pstr);
+void puts_half(char *pstr);
+void print_array(int *pa, int n);
+char *_strcpy(char *pdest, char *psrc);
+int _atoi(char *ps);
 
 #endif /* MAIN_H */
