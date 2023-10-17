24f71d3e246d0fa26e550bef98c3d99f44302ade
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-10-17 17:26:48 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -28,3 +28,25 @@ void print_array(int *pa, int n)
 		}
 	}
 }
+/**
+  * _strlen - a function
+  * @ps: a pointer to a string
+  * Description: returns the length of a string
+  * Programmer: Isaac-ik
+  * Return: length of the string
+  */
+int _strlen(char *ps)
+{
+	int length;
+	char *pi;
+
+	length = 0;
+	pi = ps;
+
+	while (*pi != '\0')
+	{
+		length = length + 1;
+		pi = pi + 1;
+	}
+	return (length);
+}
