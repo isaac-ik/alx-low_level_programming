00540b56549776fa035f23b07584f6784f58df36
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-10-17 16:53:42 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -28,3 +28,26 @@ void rev_string(char *ps)
 	}
 	_putchar('\n');
 }
+
+/**
+  * _strlen - a function
+  * @ps: a pointer to a string
+  * Description: returns the length of a string
+  * Programmer: Isaac-ik
+  * Return: length of the string
+  */
+int _strlen(char *ps)
+{
+	int length;
+	char *pi;
+
+	length = 0;
+	pi = ps;
+
+	while (*pi != '\0')
+	{
+		length = length + 1;
+		pi = pi + 1;
+	}
+	return (length);
+}
