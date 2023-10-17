81243565b706d9f0b5c8a3d37effa43824eede48
isaac-ik <123403214+isaac-ik@users.noreply.github.com>
2023-10-17 03:23:44 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int _strlen(char *ps);
 /**
   * puts2 - a function
   * @pstr - string
@@ -18,4 +19,26 @@ void puts2(char *pstr)
 		ev = ev + 2;
 	}
 	_putchar('\n');
+}
+/**
+  * _strlen - a function
+  * @ps: a pointer to a string
+  * Description: returns the length of a string
+  * Programmer: Isaac-ik
+  * Return: length of the string
+  */
+int _strlen(char *ps)
+{
+	int length;
+	char *pi;
+
+	length = 0;
+	pi = ps;
+
+	while (*pi != '\0')
+	{
+		length = length + 1;
+		pi = pi + 1;
+	}
+	return (length);
 }
