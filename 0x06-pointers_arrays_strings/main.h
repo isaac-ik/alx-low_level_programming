c76d2cd9fc6e963356c45e0da5b4acc4a6b5ae2c
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-18 05:04:39 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *pdest, char *psrc);
+char *_strncat(char *pdest, char *psrc, int n);
+char *_strncpy(char *pdest, char *psrc, int n);
+int _strcmp(char *ps1, char *ps2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+
+#endif /* MAIN_H */
