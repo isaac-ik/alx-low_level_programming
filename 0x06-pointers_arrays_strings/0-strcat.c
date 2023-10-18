a2d7af82295f6f16cc5185f87b3c15d78115244f
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-18 05:22:00 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _strcat - concentates two strings
+  * @pdest: first string
+  * @psrc: second string
+  * Porgrammer: Isaac-ik
+  * return: the concentated string
+  */
+char *_strcat(char *pdest, char *psrc)
+{
+}
+
