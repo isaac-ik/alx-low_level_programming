22256e0367bf7ac82418a4e1f14a9286ebbed80e
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-23 06:58:44 -0700

    even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,6 +6,9 @@
   * @c: char to find in string
   * Description: Returns a pointer to the first occurrence of the character c
   * in the string s, or NULL if the character is not found
+  * The terminating null byte is considered part of the string, so that
+  * if c is specified as '\0', these functions return a pointer to
+  * the terminator.
   * Programmer: Isaac-ik
   * Return: address of the first occurence of c, otherwise NULL
   */
@@ -14,16 +17,20 @@ char *_strchr(char *ps, char c)
 	int i;
 
 	/* loop through the string */
-	for (i = 0; *(ps + i) != '\0'; i++)
+	for (i = 0; *(ps + i) != '\0' || *(ps + i) == '\0'; i++)
 	{
 		/* check if its a match */
 		if (*(ps + i) == c)
 		{
 			return (ps + i);
 		}
-		else
+		else if (*(ps + i) != '\0' && *(ps + i) != c)
 		{
 			continue;
+		}
+		else
+		{
+			return ('\0');
 		}
 	}
 	return ('\0');
