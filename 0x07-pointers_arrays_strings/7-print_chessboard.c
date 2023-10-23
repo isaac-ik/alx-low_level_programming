2feba84b91df1d42f0132cc6b966c78694a573c8
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-23 16:19:24 -0700

    pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,14 +12,19 @@ void print_chessboard(char (*pa)[8])
 
 	z = 0;
 
-	for (i = 0; (*pa)[i] != '\0'; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (z = 0; z <= 8; z++)
+		for (z = 0; z < 8; z++)
 		{
-			if (z == 8)
-				_putchar('\n');
-			else
+			if (z == 7)
+			{
 				_putchar(pa[i][z]);
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(pa[i][z]);
+			}
 		}
 	}
 }
