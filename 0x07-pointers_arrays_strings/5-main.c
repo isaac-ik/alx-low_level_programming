db0c1ef7acef6b5d51672406fbbea59f888467b0
isaac-ik <isaac.ikpemi@gmail.com>
2023-10-23 17:15:12 -0700

    pointers

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    char *s = "hello, worldlove";
-    char *f = "love";
+    char *s = "hello, world";
+    char *f = "world";
     char *t;
 
-    t = _strpbrk(s, f);
+    t = _strstr(s, f);
     printf("%s\n", t);
     return (0);
 }
