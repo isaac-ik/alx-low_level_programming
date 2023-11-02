034513b986ecb806133bec63facd43295cca8a2f
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-02 08:04:46 -0700

    more memoory allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,12 +3,47 @@
 #include <stdlib.h>
 
 /**
- *  -
- *  Description:
- *  Programmer:
- *  @
- *  @
- *  @
- *  Return:
+ *  string_nconcat - function
+ *  Description: concatenates two strings.
+ *  eturned pointer shall point to a newly allocated space in memory,
+ *  which contains s1, followed by the first n bytes of s2, and null terminated
+ *  Programmer: Isaac-ik
+ *  @ps1: first string
+ *  @ps2: second string
+ *  @n: the number of the second string to concentate
+ *  Return: pointer to new string, or NULL if any problem
  */
+char *string_nconcat(char *ps1, char *ps2, unsigned int n)
+{
+	unsigned index, i, s;
+	char *pnew;
+	
+	if (n >= sizeof(ps2) - 1)
+	{
+		s = sizeof(ps2) - 1;
+	}
+	else
+	{
+		s = n;
+	}
 
+	pnew = (char *)malloc(sizeof(ps1) + s - 1);
+	if (pnew == NULL)
+	{
+		return (NULL);
+	}
+
+	index = 0;
+	for (i = 0; ps1[i] != '\0'; i++)
+	{
+		pnew[index] = ps1[i];
+		index++;
+	}
+	for (i = 0; i <= s; i++)
+	{
+		pnew[index] = ps2[i];
+		index++;
+	}
+	pnew[index] = '\0';
+	return (pnew);
+}
