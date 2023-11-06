a6e34423918b5001c7c301597f301bcd7591f8c4
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-06 09:41:07 -0800

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure
+ * @name: name pointer
+ * @age: age
+ * @owner: pointer to the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
