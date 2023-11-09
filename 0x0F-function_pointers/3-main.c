07bd58a351ade6d0c4000ad90bbab645e45a90bd
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-08 20:10:19 -0800

    Fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,6 +13,7 @@
 int main(int argc, char **argv)
 {
 	int num1, num2, result;
+	char *operator;
 
 	/* Check if no of argumnet is not 4 */
 	if (argc > 4 || argc < 4 || atoi(argv[1]) == 0 || atoi(argv[3]) == 0)
@@ -24,18 +25,20 @@ int main(int argc, char **argv)
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	operator = argv[2];
+
+	if ((*operator == '/' || *operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(argv[2]) == NULL)
+	if (get_op_func(operator) == NULL)
 	{
 		/* The operator is not among the defined ones */
 		printf("Error\n");
 		exit(99);
 	}
-	result = get_op_func(argv[2])(num1, num2);
+	result = get_op_func(operator)(num1, num2);
 	printf("%d\n", result);
 	return (result);
 }
