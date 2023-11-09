46df0bb7e18941299ec41eec00b265b9a42c59dd
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-08 19:10:40 -0800

    Fix Error in Function pointers in Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char **argv)
 {
-	long int num1, num2, result;
+	int num1, num2, result;
 
 	/* Check if no of argumnet is not 4 */
 	if (argc > 4 || argc < 4 || atoi(argv[1]) == 0 || atoi(argv[3]) == 0)
@@ -23,15 +23,21 @@ int main(int argc, char **argv)
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	
-	result = get_op_func(argv[2])(num1, num2);
-	if (result == -1)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		/* The operator is not among the defined ones */
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%l\n", result);
+	result = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", result);
 	
 	return (result);
 }
