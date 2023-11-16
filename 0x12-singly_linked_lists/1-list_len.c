b7a8f3321047fece3f382dca0486ea92e5924010
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-16 09:19:51 -0800

    single linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,29 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stddef.h>
-
+#include <stdio.h>
 /**
- * i - a function
+ * list_len - a function
  * Programmer: Isaac-ik
- * Description: 
- * @:
- * Return:
+ * Description: returns the number of elements in a linked list_t list.
+ * @h: pointer to the first elemnet on the linked list
+ * Return: 0
  */
-list_t
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (1)
+	{
+		if (h->next == NULL)
+		{
+			count++;
+			break;
+		}
+		else
+			count++;
+		h = h->next;
+	}
+
+	return (count);
+}
