96135fdd224633f531aa9d3983d759172dd4b18d
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-16 08:35:37 -0800

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - a function
+ * Programmer: Isaac-ik
+ * Description: prints all the elements of a list_t list.
+ * @h: a struct (given the properties of a singly linked list)
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (1)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		if (h->next == NULL)
+			break;
+		h = h->next;
+	}
+
+	return (count);
+
+}
