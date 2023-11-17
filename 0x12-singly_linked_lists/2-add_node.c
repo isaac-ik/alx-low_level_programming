8605be7b0cd99ec492a8525547b18bb20eb0af1e
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-16 19:12:29 -0800

    singly linked list adding a node to the the lis

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,29 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stddef.h>
-
+#include <stdio.h>
+#include <string.h>
 /**
- * i - a function
+ * add_node - a function
  * Programmer: Isaac-ik
- * Description: 
- * @:
- * Return:
+ * Description: adds a new node at the beginning of a list_t list
+ * @head: pointer to new head
+ * Return: address of new element
  */
-list_t
+list_t *add_node(list_t **head, const char *str)
+{
+	char *newstr;
+	list_t *currentNode;
+
+	newstr = strdup(str);
+
+	currentNode = malloc(sizeof(list_t));
+	if (currentNode == NULL)
+		return (NULL);
+	currentNode->str = newstr;
+	currentNode->len = strlen(newstr);
+	currentNode->next = *head;
+	*head = currentNode;
+
+	return (currentNode);
+}
