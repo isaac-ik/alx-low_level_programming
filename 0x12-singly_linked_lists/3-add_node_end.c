98fa9df08c978c1fdaccb6313f240825a205eed0
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-16 19:39:54 -0800

    singly linked list adding a node to the end of the list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,12 +1,39 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stddef.h>
-
+#include <stdio.h>
+#include <string.h>
 /**
- * i - a function
+ * add_node_end - a function
  * Programmer: Isaac-ik
- * Description: 
- * @:
- * Return:
+ * Description: adds a new node at the beginning of a list_t list
+ * @head: pointer to new head
+ * Return: address of new element
  */
-list_t
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *newstr;
+	list_t *currentNode;
+	list_t *pnext;
+
+	pnext = *head;
+	newstr = strdup(str);
+
+	currentNode = malloc(sizeof(list_t));
+	if (currentNode == NULL)
+		return (NULL);
+	while (1)
+	{
+		if (pnext->next == NULL || pnext == NULL)
+		{
+			pnext->next = currentNode;
+			break;
+		}
+		pnext = pnext->next;
+	}
+	currentNode->str = newstr;
+	currentNode->len = strlen(newstr);
+	currentNode->next = NULL;
+
+	return (currentNode);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * i - a function
+ * Programmer: Isaac-ik
+ * Description: 
+ * @:
+ * Return:
+ */
+list_t
