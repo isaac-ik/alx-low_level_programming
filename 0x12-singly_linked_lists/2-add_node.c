673012cfb53b6ffacf79603c86e0b476c76ff477
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-17 13:55:58 -0800

    correctioon to linked list adddition to the beginnning file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,6 +8,7 @@
  * Programmer: Isaac-ik
  * Description: adds a new node at the beginning of a list_t list
  * @head: pointer to new head
+ * @str: node to be added to the begining
  * Return: address of new element
  */
 list_t *add_node(list_t **head, const char *str)
@@ -15,6 +16,8 @@ list_t *add_node(list_t **head, const char *str)
 	char *newstr;
 	list_t *currentNode;
 
+	if (str == NULL)
+		return (NULL);
 	newstr = strdup(str);
 
 	currentNode = malloc(sizeof(list_t));
