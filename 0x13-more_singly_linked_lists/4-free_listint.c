579188c4d8c7f78a8e517bbebf1b08da86b95143
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-20 07:54:40 -0800

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - a linked list function
+ * Programmer: Isaac-ik
+ * Description: frees a listint_t list.
+ * @head: address of beginning to the list
+ * Return: the number of nodes
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *p = head;
+	listint_t *j;
+
+	/* if the next node is points to nothing */
+	if (p->next == NULL)
+	{
+		free(p); /* just free the current node */
+		exit(1);
+	}
+
+	/* if the next node points to something */
+	if (p->next != NULL)
+	{
+		j = p->next; /* assign the next node to j */
+		free(p); /* free the current node */
+		free_listint(j); /* call the free_listint function on j */
+		exit(1);
+	}
+}
