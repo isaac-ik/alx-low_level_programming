312c4019cc5f6f380d974037970f69bccdacaa93
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-20 10:16:50 -0800

    that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,34 @@
 #include <stdlib.h>
 
 /**
+ * get_nodeint_at_index - a linked list function
+ * Programmer: Isaac-ik
+ * Description: returns the nth elements of a listint_t list.
+ * @head: pointer to the list
+ * @index: index
+ * Return: address to node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node = 0;
+	listint_t *p = head;/* assign it the address of the list */
+
+	if (p == NULL)
+		return (NULL);
+	/* the address isnt pointing to a NULL */
+	while (p != NULL || index >= node)
+	{
+		if (index == node)
+		{
+			return (p);
+		}
+		node++;
+		p = p->next;
+	}
+	return (NULL);
+}
+
+/**
  * insert_nodeint_at_index - a linked list function
  * Programmer: Isaac-ik
  * Description: adds a new element to the index in a listint_t list.
