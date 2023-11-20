67d7207666f9c2b5d4e83abb0b02249f23e16142
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-20 08:02:10 -0800

    frees a listint_t list and assigns the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - a linked list function
+ * Programmer: Isaac-ik
+ * Description: frees a listint_t list, then assigns head to NULL
+ * @head: address of beginning to the list
+ * Return: the number of nodes
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p = *head;
+	listint_t *j;
+
+	/* if the next node is points to nothing */
+	if (p->next == NULL)
+	{
+		free(p); /* just free the current node */
+		*head = NULL;
+		exit(1);
+	}
+
+	/* if the next node points to something */
+	if (p->next != NULL)
+	{
+		j = p->next; /* assign the next node to j */
+		free(p); /* free the current node */
+		free_listint2(&j); /* call the free_listint function on j */
+		exit(1);
+	}
+}
