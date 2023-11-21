d1ee8aa6251879ebecd57837794b5462ad445c35
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-21 05:07:13 -0800

    multiplies two positive numbers given as program args

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,12 +3,44 @@
 #include <stdlib.h>
 
 /**
- *  -
- *  Description:
- *  Programmer:
- *  @
- *  @
- *  @
- *  Return:
+ *  main - main fucntion
+ *  Description: takes in two +ve args and multiplies them.
+ *  Programmer: Isaac-ik
+ *  @argc: number of argumnets
+ *  @argv: argument vector
+ *  Return: result
  */
+int main(int argc, char **argv)
+{
+	int index, c, num1, num2, result;
+	/* if number of args is incorrect */
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/* check if it contains only digits */
+	index = 1;
+	while (index < argc)
+	{
+		c = 0;
+		while (argv[index][c] != '\0')
+		{
+			if (argv[index][c] < 48 || argv[index][c] > 57)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			c++;
+		}
+		index++;
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
+	result = num1 * num2;
+
+	printf("The result is %d\n", result);
+
+	return (result);
+}
