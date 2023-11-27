672beca8ef03b994cedb81a695ef69ee9eccd222
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-27 10:32:13 -0800

    File I/O: modification to 1-create.. 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *pfilename, char *ptext_content)
 {
 	int fileD, s;
 
-	if (filename == NULL)
+	if (pfilename == NULL)
 		return (-1);
 	/* Open a file */
 	fileD = open(pfilename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
