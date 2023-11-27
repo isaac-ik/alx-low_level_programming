fbc1f773a8934b68fd00ea921026edd9201e59ab
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-27 10:20:01 -0800

    File I/O: 1-create... create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * len - gets the length of a str
+ * @pstr: pointer to string
+ * Programmer: Isaac-ik
+ * Return: length of string
+ */
+int len(char *pstr)
+{
+	int count = 0, i;
+
+	if (pstr == NULL)
+		return (0);
+	for (i = 0; pstr[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * create_file -  creates a file
+ * @pfilename: points to filename
+ * @ptext_content: pointer to the content
+ * Programmer: Isaac-ik
+ * Return: 1, on sucess or any other num depending on the encountred error
+ */
+int create_file(const char *pfilename, char *ptext_content)
+{
+	int fileD, s;
+
+	/* Open a file */
+	fileD = open(pfilename, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fileD < 0)
+	{
+		close(fileD);
+		return (-1);
+	}
+
+	/* write the content into the file */
+	s = write(fileD, ptext_content, len(ptext_content));
+	if (s < 0)
+		return (-1);
+
+	close(fileD);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 
 ssize_t read_textfile(const char *pfilename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *pfilename, char *ptext_content);
+int append_text_to_file(const char *pfilename, char *ptext_content);
+
+int len(char *pstr);
 
 #endif
